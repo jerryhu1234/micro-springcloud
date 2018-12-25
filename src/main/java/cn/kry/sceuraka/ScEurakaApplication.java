20dcfd4b4fae71de746343cf258af745b12b1f50
huming <jerry_hu123@163.com>
2018-12-25 16:55:03 +0800

    Commit

---
+++ src/main/java/cn/kry/sceuraka/ScEurakaApplication.java
@@ -0,0 +1,14 @@
+package cn.kry.sceuraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScEurakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScEurakaApplication.class, args);
+	}
+
+}
+
