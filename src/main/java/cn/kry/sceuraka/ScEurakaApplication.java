7161cdc6a83d7e6c255da111511a8c1180143278
huming <jerry_hu123@163.com>
2018-12-25 17:09:43 +0800

    fixed

--- src/main/java/cn/kry/sceuraka/ScEurakaApplication.java
+++ src/main/java/cn/kry/sceuraka/ScEurakaApplication.java
@@ -2,8 +2,16 @@ package cn.kry.sceuraka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
+public class Sb2scEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Sb2scEurekaApplication.class, args);
+    }
+}
 public class ScEurakaApplication {
 
 	public static void main(String[] args) {
